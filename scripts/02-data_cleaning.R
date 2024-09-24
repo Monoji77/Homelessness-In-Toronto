73c358d6000c86b737cc83e9260b5d536a48c733
Chris <chrisyong2009@live.com>
2024-09-24 16:24:43 -0400

    Cleaning Data Script
    Performed simple cleaning on 'unedited_data.csv'. Changes include ensuring correct conversion of data types such as dates  and removing missing values. The resulting cleaned data is saved as 'analysis_data.csv'.

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,62 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Cleans the unclean data recorded by Toronto Shelter & Support Services 
+# Author: Chris Yong Hong Sen
+# Date: 24 September 2024
+# Contact: luke.yong@mail.utoronto.ca
 
 #### Workspace setup ####
 library(tidyverse)
+library(janitor)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
+#1 drop unnessesary columns and convert all column names to lower
+raw_data <- read_csv("../data/raw_data/unedited_data.csv")
+
+intermediate_data <- raw_data |>
   janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  select(occupancy_date, organization_id, 
+         organization_name, location_id, 
+         location_name, location_city,
+         sector, overnight_service_type,
+         program_area, service_user_count,
+         capacity_actual_bed, capacity_funding_bed, 
+         unoccupied_beds, occupancy_rate_beds) %>%
+  
+  # carefully handle dates since structure across years are different
+  mutate(occupancy_date = case_when(
+    
+    substring(occupancy_date,1, 4) %in% c('2023', '2024') ~ substring(occupancy_date, 1, 10),
+      .default = paste0('20', substring(occupancy_date, 1, 8))),
+    
+    occupancy_date = as.Date(occupancy_date))
+
+
+#2 check for missing values
+columns_na <- names(intermediate_data)[colSums(is.na(intermediate_data)) > 0]
+missing_values_count <- function(data) {
+  
+  #obtain columns with NA
+  columns_na <- names(data)[colSums(is.na(data)) > 0]
+  
+  #obtain counts for each column with missing values
+  for (column_with_missing_values in columns_na) {
+    missing_count_for_column <- sum(is.na(data[column_with_missing_values]))
+    print(paste(column_with_missing_values, missing_count_for_column, sep=': '))
+  }
+}
+
+missing_values_count(intermediate_data)
+
+#3 drop missing values: missing values have to be dropped. It does not make sense to interpolate 
+#                       values nor to use the mean/median to substitute as we don't know what
+#                       other factors could affect the capacity of the area.
+# 
+# NOTE: by dropping values, we lost approximately 35% of information.
+
+cleaned_data <- intermediate_data |> drop_na()
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "../data/analysis_data/analysis_data.csv")
+
