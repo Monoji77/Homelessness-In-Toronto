d719900347e4f080a7c3078dc044823304358c5e
Chris <chrisyong2009@live.com>
2024-09-24 05:53:35 -0400

    Simulating Data
    Wrote an R script allowing us to simulate data that is parallel to the 'Daily Shelter & Overnight Service Occupancy & Capacity' dataset by 'Open Data Toronto'. The resulting simulated data can be used for summarised statistical analysis.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,38 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Purpose: Simulates Daily Shelter & Overnight Service Occupancy & Capacity dataset
+# Author: Chris Yong Hong Sen
+# Date: 24 September 2024
+# Contact: luke.yong@mail.utoronto.ca
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(lubridate)
+set.seed(517)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+simulated_data <- tibble(
+  occupancy_date = sample(seq(as.Date('2024-01-01'), Sys.Date(), by='days'), 15, replace=TRUE),
+  organisation_id = sample(1:3, 15, replace=TRUE),
+  location_id = sample(100:105, 15, replace=TRUE),
+  location_city = sample(c('Toronto', 'Calgary', 'Vancouver'), 15, replace=TRUE),
+  homeless_people_served = sample(1:1500, 15, replace=TRUE)
+)
 
+#### Tests ####
 
+# test whether date is within the year 2024
+simulated_data |>
+  filter(
+    !(occupancy_date %in% seq(as.Date('2024-01-01'), as.Date('2024-12-31'), by='days'))
+  ) |>
+  nrow() == 0
+
+# test whether number of homeless people served is at least 0
+sum(simulated_data$homeless_people_served < 0) == 0
+
+# test whether homeless shelters are all within Canadian cities
+sum(
+  !(simulated_data$location_city %in% 
+    c('Toronto', 'Calgary', 'Vancouver', 'Edmonton', 'Winnipeg', 'Thunder Bay'))
+) == 0
 
